99c3bb894c2b93bfff707623e3516741250dedd8
Parfionov Anton <toystix1992@gmail.com>
2021-03-15 00:13:38 +0300

    Feat:Add sol to count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+  if(matrix.length === 0) {
+    return 0;
+  }
+  let catsArr = matrix.reduce((acc, val) => acc.concat(val)).filter(item=> item === '^^');
+  if (catsArr.length > 0) {
+    return catsArr.length * 1;
+  } else {
+    return 0;
+  }
+}
